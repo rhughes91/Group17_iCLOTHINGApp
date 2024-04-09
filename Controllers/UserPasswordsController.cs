d570b6dc6779d7d5c11a7d0625f4a887168210f6
Ryan Hughes <rohughes91@gmail.com>
2024-04-08 23:44:54 -0500

    Changes.

--- Controllers/UserPasswordsController.cs
+++ Controllers/UserPasswordsController.cs
@@ -73,7 +73,6 @@ namespace Group17_iCLOTHINGApp.Controllers
             return View(result);
         }
 
-
         public ActionResult Register()
         {
             return View();
@@ -146,12 +145,15 @@ namespace Group17_iCLOTHINGApp.Controllers
         public ActionResult LoggedIn()
         {
             if (!Verified())
+            {
                 return RedirectToAction("Index");
+            }
 
             Account account = new Account();
             if (currentUser == "admin")
             {
                 account.customer = null;
+                ViewBag.accounts = db.Customer.ToList();
                 return View(account);
             }
 
@@ -187,6 +189,150 @@ namespace Group17_iCLOTHINGApp.Controllers
             db.SaveChanges();
 
             return View("~/Views/Home/Index.cshtml");
+        }
+
+        // GET: Departments/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Account account = new Account();
+            account.customer = db.Customer.Where(c => c.userID == id).FirstOrDefault();
+            account.password = db.UserPassword.Find(id).userEncryptedPassword;
+            if (account.customer == null || account.password == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Account account)
+        {
+            UserPassword up = db.UserPassword.Find(account.customer.userID);
+            up.userEncryptedPassword = account.password;
+
+            db.Entry(up).State = EntityState.Modified;
+            db.Entry(account.customer).State = EntityState.Modified;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Departments/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Account account = new Account();
+            account.customer = db.Customer.Where(c => c.userID == id).FirstOrDefault();
+            account.password = db.UserPassword.Find(id).userEncryptedPassword;
+            if (account.customer == null || account.password == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Departments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            UserPassword up = db.UserPassword.Find(id);
+            Customer customer = db.Customer.Where(c => c.userID == id).FirstOrDefault();
+
+            List<ShoppingCart> carts = db.ShoppingCart.Where(sc => sc.customerID == customer.customerID).ToList();
+            List<Email> emails = db.Email.Where(e => e.customerID == customer.customerID).ToList();
+            List<UserComment> comments = db.UserComment.Where(e => e.userID == customer.userID).ToList();
+            List<UserQuery> queries = db.UserQuery.Where(e => e.searchID == customer.userID).ToList();
+
+            foreach (ShoppingCart cart in carts)
+            {
+                List<ItemDelivery> deliveries = db.ItemDelivery.Where(d => d.cartID == cart.cartID).ToList();
+                foreach (ItemDelivery delivery in deliveries)
+                {
+                    db.ItemDelivery.Remove(delivery);
+                }
+                db.ShoppingCart.Remove(cart);
+            }
+            foreach (Email email in emails)
+            {
+                db.Email.Remove(email);
+            }
+            foreach (UserComment comment in comments)
+            {
+                db.UserComment.Remove(comment);
+            }
+            foreach (UserQuery query in queries)
+            {
+                db.UserQuery.Remove(query);
+            }
+
+            db.UserPassword.Remove(up);
+            db.Customer.Remove(customer);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Account result)
+        {
+            var query = from user in db.UserPassword where user.userID == result.customer.userID select user;
+            if (result.valid() && query.Any())
+            {
+                System.Diagnostics.Debug.WriteLine("Username has already been taken");
+                return View(result);
+            }
+
+            UserPassword up = new UserPassword();
+            up.userID = result.customer.userID;
+            up.userAccountName = result.customer.userID;
+            up.userEncryptedPassword = result.password;
+            up.passwordExpiryTime = 31;
+            up.userAccountExpirtDate = DateTime.Now.AddDays(up.passwordExpiryTime);
+
+            Customer cust = result.customer;
+            cust.customerBillingAddress = "";
+            cust.customerShippingAddress = "";
+            switch (cust.customerGender.ToLower())
+            {
+                case "m":
+                case "man":
+                case "male":
+                    cust.customerGender = "M";
+                    break;
+                case "f":
+                case "woman":
+                case "female":
+                    cust.customerGender = "F";
+                    break;
+                default:
+                    cust.customerGender = "X";
+                    break;
+            }
+
+            db.UserPassword.Add(up);
+            db.Customer.Add(cust);
+            db.SaveChanges();
+
+            return RedirectToAction("LoggedIn");
         }
 
         protected override void Dispose(bool disposing)
